7cb82c865288167eadfd1edd84c557cd695738e0
alec <ajhende4@ncsu.edu>
2024-06-25 21:05:30 -0400

    made autodeploy script better

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -9,4 +9,7 @@ cd /home/alec/ClubTennis
 
 LOCK_FILE="$(pwd)/deyployment.lock"
 rm ./deployment.log
-flock -n $LOCK_FILE ./scripts/rebuild_if_changed.sh >> ./deployment.log 2>&1
+
+TTL=$( { time flock -n $LOCK_FILE ./scripts/rebuild_if_changed.sh >> ./deployment.log 2>&1; } 2>&1 )
+
+echo "$(date --utc +%FT%TZ): Build completed in $TTL" >> ./deployment_result.log
