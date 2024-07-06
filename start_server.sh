ca5136d8896bc3be35da4652d56feb69ad9e6ed7
alec <alec@localhost.members.linode.com>
2024-07-06 16:33:37 -0500

    update start server for partial image

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd /home/alec/ClubTennis
+if [ -f ./scripts/build_base_image.sh ]; then
+	./scripts/build_base_image.sh
+fi
+
 docker compose up --build > ./server.log 2>&1 &
 echo $! | sudo tee /run/docker-webserver.pid > /dev/null
